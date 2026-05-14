b6b3723080b3741df82fd343f9b43db020be21bb
Jakub Juracka <jakub.juracka@cern.ch>
2026-05-15 00:34:40 +0200

    simplify header

--- Modules/MFT/include/MFT/QcMFTClusterPostProcessing.h
+++ Modules/MFT/include/MFT/QcMFTClusterPostProcessing.h
@@ -10,59 +10,44 @@
 // or submit itself to any jurisdiction.
 
 ///
-/// \file   SkeletonPostProcessing.h
-/// \author My Name
+/// \file   QcMFTClusterPostProcessing.h
+/// \author Jakub Juracka
 ///
 
-#ifndef QUALITYCONTROL_SKELETONPOSTPROCESSING_H
-#define QUALITYCONTROL_SKELETONPOSTPROCESSING_H
+#ifndef QUALITYCONTROL_QCMFTCLUSTERPOSTPROCESSING_H
+#define QUALITYCONTROL_QCMFTCLUSTERPOSTPROCESSING_H
 
 #include "QualityControl/PostProcessingInterface.h"
 #include <memory>
+#include <vector>
 #include <TH1F.h>
+#include <TCanvas.h>
+#include <THStack.h>
+#include <TColor.h>
 
-namespace o2::quality_control_modules::skeleton
+namespace o2::quality_control_modules::mft
 {
 
-/// \brief Example Quality Control Postprocessing Task
-/// \author My Name
-class SkeletonPostProcessing final : public quality_control::postprocessing::PostProcessingInterface
+/// \brief Comparative visualisation of MFT cluster positions across all detector layers.
+class QcMFTClusterPostProcessing final : public quality_control::postprocessing::PostProcessingInterface
 {
  public:
   /// \brief Constructor
-  SkeletonPostProcessing() = default;
+  QcMFTClusterPostProcessing();
   /// \brief Destructor
-  ~SkeletonPostProcessing() override;
+  ~QcMFTClusterPostProcessing() override;
 
-  /// \brief Configuration a post-processing task.
-  /// Configuration of a post-processing task. The task may create variables that shall live throughout its lifetime.
-  /// \param config boost property with the full QC configuration file
+  // Definition of the methods
   void configure(const boost::property_tree::ptree& config) override;
-  /// \brief Initialization of a post-processing task.
-  /// Initialization of a post-processing task. User receives a Trigger which caused the initialization and a service
-  /// registry with singleton interfaces.
-  /// \param trigger  Trigger which caused the initialization, for example Trigger::SOR
-  /// \param services Interface containing optional interfaces, for example DatabaseInterface
   void initialize(quality_control::postprocessing::Trigger, framework::ServiceRegistryRef) override;
-  /// \brief Update of a post-processing task.
-  /// Update of a post-processing task. User receives a Trigger which caused the update and a service
-  /// registry with singleton interfaces.
-  /// \param trigger  Trigger which caused the initialization, for example Trigger::Period
-  /// \param services Interface containing optional interfaces, for example DatabaseInterface
   void update(quality_control::postprocessing::Trigger, framework::ServiceRegistryRef) override;
-  /// \brief Finalization of a post-processing task.
-  /// Finalization of a post-processing task. User receives a Trigger which caused the finalization and a service
-  /// registry with singleton interfaces.
-  /// \param trigger  Trigger which caused the initialization, for example Trigger::EOR
-  /// \param services Interface containing optional interfaces, for example DatabaseInterface
   void finalize(quality_control::postprocessing::Trigger, framework::ServiceRegistryRef) override;
 
  private:
-  std::unique_ptr<TH1F> mHistogramA = nullptr; // lives through the whole task lifetime
-  std::unique_ptr<TH1F> mHistogramB = nullptr; // lives until the end of each run
-  std::unique_ptr<TH1F> mHistogramC = nullptr; // lives for one-time publications
+  std::unique_ptr<TCanvas> mCanvas = nullptr;
+  std::unique_ptr<THStack> mStack = nullptr;
 };
 
-} // namespace o2::quality_control_modules::skeleton
+} // namespace o2::quality_control_modules::mft
 
-#endif //QUALITYCONTROL_SKELETONPOSTPROCESSING_H
+#endif //QUALITYCONTROL_QCMFTCLUSTERPOSTPROCESSING_H
